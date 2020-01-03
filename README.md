72a679284d464f80dd84ad936415c423e20b02ed
Daewoong Bang <daewoong.bang@gmail.com>
2020-01-03 16:01:54 +0900

    api 호출하여 movie 기본 데이터 가져오기

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Movie App 2019
 
+리액트에 대한 기본 개념을 익히기 위한 미니 Project
+
 React JS Fundamentals Course (2019)
+
+1. Setup
+
+2. JSX & Props
+
+3. State
--- src/App.js
+++ src/App.js
@@ -1,7 +1,59 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
 
-function App() {
-  return <div className="App">hello</div>;
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+
+  // 비동기 함수 - 해당 함수를 기다려라
+  getMovies = async () => {
+    // axios가 끝날 때까지 기다렸다가 계속 진행.
+    // movies.data.data.movies
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    // console.log(movies);
+    this.setState({ movies, isLoading: false });
+  };
+
+  // render()가 끝난 후 호출
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
+  }
 }
 
 export default App;
